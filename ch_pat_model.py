7289beacdaac5b9d5596e934b4e2365c91375ca2
Sergey Kovalchuk <sergey.v.kovalchuk@gmail.com>
2019-10-15 18:12:18 +0300

    Reduce comments

--- ch_pat_model.py
+++ ch_pat_model.py
@@ -5,24 +5,14 @@ class ChPatModel:
     """Basic class for personalized presiction."""
 
     def check_applicability(self, patient_dict):
-        """Check for applicability of the model against particular patient.
-
-        Arguments:
-            patient_dict {dictionary} -- Patient parameters
-        """
+        """Check for applicability of the model against particular patient described in patient_dict."""
         pass
 
     def apply(self, patient_dict):
-        """Apply and extend the dictionary for a patient.
-        
-        Arguments:
-            patient_dict {dictionary} -- Input dictionary for a patient            
-
-        Returns:
-            [dictionary] -- Output (extended) dictionary
-        """
+        """Apply and extend the dictionary for a patient (patient_dict)."""
         return patient_dict.copy()
 
     def store_model(self, fname):
+        """Store the model in pickle file"""
         pickle.dump(self, open(fname, 'wb'))
 
