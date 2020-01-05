f2c18caddc1edacbf64b57082f49ec25adf00e4d
iterater <sergey.v.kovalchuk@gmail.com>
2020-01-05 23:52:26 +0300

    Update experimental_run.py

--- experimental_run.py
+++ experimental_run.py
@@ -1,5 +1,7 @@
 import requests
 import timeit
+import joblib
+import datetime
 from ch_pat_models_management import load_params, generate_default_data
 
 params = load_params('params_list.csv')
@@ -8,8 +10,18 @@ url = 'http://195.222.181.179:5000/ch_pat_service'
     
 def request():        
     resp = requests.post(url, json=in_dict)
+    return resp.status_code == 200
+
+def single_series(n, i):
+    print('Run {} with {} requests'.format(i, n))
+    t = timeit.timeit(request, number=n)
+    return t
 
 if __name__ == '__main__':
-    t = timeit.timeit(request, number=100)
-    print(t)
+    for n_par in range(1,7):
+        start_date_time = datetime.datetime.now()
+        run_res = joblib.Parallel(n_jobs=n_par)(joblib.delayed(single_series)(100, i) for i in range(16))
+        stop_date_time = datetime.datetime.now()        
+        dt = (stop_date_time - start_date_time) / datetime.timedelta(milliseconds=1)
+        print('{}, {}, {}'.format(n_par, dt, ', '.join(run_res)))
 
